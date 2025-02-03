8929da2e1927d15d666c33adb63c2d2034d5ac6e
Swetha shree.S <swethasundaram2005@gmail.com>
2025-02-03 21:41:29 +0530

    Update bookroutes for get,put,delete

--- routes/bookroutes.js
+++ routes/bookroutes.js
@@ -3,5 +3,8 @@ const router = require('express').Router()
 
 router.get('/api/books', bookController.getAllBooks);
 router.get('/api/book/:id', bookController.getSingleBook);
+router.post('/api/book', bookController.addNewBook);
+router.put('/api/book', bookController.updateBook);
+router.delete('/api/book/:id', bookController.deleteBook);
 
 module.exports=router
